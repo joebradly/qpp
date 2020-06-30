b940c8464e12ca01c918debc158e33249f0d71b9
Vlad Gheorghiu <vsoftco@gmail.com>
2020-06-30 17:05:37 -0400

    Update _test.cpp

--- examples/_test.cpp
+++ examples/_test.cpp
@@ -4,11 +4,28 @@
 #include "qpp.h"
 #include "experimental/experimental.h"
 
-int main() {
+int main(int argc, char** argv) {
     /////////// testing ///////////
     using namespace qpp;
-    try {
-    } catch (std::exception& e) {
-        std::cout << e.what() << std::endl;
+
+    std::size_t nq = 20;
+    QCircuit qCircuit{nq, nq};
+    qCircuit.gate_fan(gt.H);
+    for (idx i = 0; i < nq - 1; ++i) {
+        qCircuit.CTRL(gt.X, i, i + 1);
     }
+    qCircuit.gate_fan(gt.H);
+    for (idx i = 0; i < nq; ++i) {
+        qCircuit.measureZ(i, i);
+    }
+
+    std::size_t reps = std::stoi(argv[1]);
+
+    QEngine qEngine{qCircuit};
+    Timer<> t;
+    qEngine.execute(reps);
+    t.toc();
+
+    std::cout << qEngine << '\n';
+    std::cout << "Took: " << t << " seconds to execute\n";
 }
